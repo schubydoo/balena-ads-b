3387c3cc1ef645951dfbf3ec7b669e5111a2a6fa
Schuby <schubydoo@users.noreply.github.com>
2023-01-08 11:11:35 -0800

    Attempting to detect the DISABLED_SERVICES variable. (#5)

---
+++ balena-dash/wifi-connect/start.sh
@@ -0,0 +1,36 @@
+#!/bin/busybox sh
+
+# Check if service has been disabled through the DISABLED_SERVICES environment variable.
+
+if [[ ",$(echo -e "${DISABLED_SERVICES}" | tr -d '[:space:]')," = *",$BALENA_SERVICE_NAME,"* ]]; then
+        echo "$BALENA_SERVICE_NAME is manually disabled."
+        sleep infinity
+fi
+
+if [[ ! -z $CHECK_CONN_FREQ ]] 
+    then
+        freq=$CHECK_CONN_FREQ
+    else
+        freq=120
+fi
+
+
+sleep 5
+
+while [[ true ]]; do
+    if [[ $VERBOSE != false ]]; then echo "Checking internet connectivity ..."; fi
+    wget --spider --no-check-certificate 1.1.1.1 > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        if [[ $VERBOSE != false ]]; then echo "Your device is already connected to the internet.\nSkipping setting up Wifi-Connect Access Point. Will check again in $freq seconds."; fi        
+    else
+        if [[ $VERBOSE != false ]]; then echo "Your device is not connected to the internet.\nStarting up Wifi-Connect.\n Connect to the Access Point and configure the SSID and Passphrase for the network to connect to."; fi        
+        DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket /usr/src/app/wifi-connect -u /usr/src/app/ui
+    fi
+
+    sleep $freq
+
+done
+
+
+/bin/busybox sh /usr/bin/balena-idle
