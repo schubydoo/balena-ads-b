eb9e849d36ddcab50ca56422b9d9a498cdcccec7
Teko012 <112829523+Teko012@users.noreply.github.com>
2025-01-24 02:39:22 +0100

    Add curl retry for supervisor

--- dump978-fa/start.sh
+++ dump978-fa/start.sh
@@ -6,7 +6,7 @@ set -e
 
 if [[ ",$(echo -e "${DISABLED_SERVICES}" | tr -d '[:space:]')," = *",$BALENA_SERVICE_NAME,"* ]] || [[ "$DUMP1090_IDLE" = "true" ]]; then
         echo "$BALENA_SERVICE_NAME is manually disabled. Sending request to stop the service:"
-        curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
+        curl --retry 10 --retry-all-errors --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
         echo " "
         balena-idle
 fi
@@ -15,7 +15,7 @@ fi
 
 if ! [[ "$UAT_ENABLED" = "true" ]]; then
         echo "$BALENA_SERVICE_NAME is not enabled. Sending request to stop the service:"
-        curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
+        curl --retry 10 --retry-all-errors --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
         echo " "
         balena-idle
 fi
@@ -88,5 +88,5 @@ wait -n
 
 if [[ "$REBOOT_DEVICE_ON_SERVICE_EXIT" == "true" ]]; then
         echo "Service exited, rebooting the device..."
-        curl -X POST --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v1/reboot?apikey=$BALENA_SUPERVISOR_API_KEY"
+        curl --retry 10 --retry-all-errors -X POST --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v1/reboot?apikey=$BALENA_SUPERVISOR_API_KEY"
 fi
