3128154275be16c52d4cbfdaa65c8df307e24bd7
schubydoo <marcus@schuby.org>
2026-04-28 18:31:04 -0700

    Autohupr Updates
    -Updated to latest code from Autohupr repo
    -Swapped to node-alpine Docker image (as used in Autohupr repo).  This will reduce size and is the first step to moving away from deprecated balenalibs base images.
    -Swapped balena-idle with sleep infinity due to above change.

--- autohupr/src/main.ts
+++ autohupr/src/main.ts
@@ -1,5 +1,18 @@
-import { getSdk, OptionalNavigationResource } from 'balena-sdk';
-import ms, { StringValue } from 'ms';
+import type { OptionalNavigationResource } from 'balena-sdk';
+import { getSdk } from 'balena-sdk';
+import type { StringValue } from 'ms';
+import ms from 'ms';
+
+/** Functional state of HUP on device for our purposes. */
+enum HupStatus {
+	RUNNING,
+	FAILED,
+	NOT_RUNNING,
+	/** Do not anticipate a device in this state; future proofing. */
+	DEVICE_BUSY,
+	/** For example, can't determine status if can't reach API. */
+	UNKNOWN,
+}
 
 const apiKey = (process.env.BALENA_API_KEY as unknown as string) ?? undefined;
 const apiUrl = (process.env.BALENA_API_URL as unknown as string) ?? undefined;
@@ -10,7 +23,7 @@ const checkInterval =
 	(process.env.HUP_CHECK_INTERVAL as unknown as StringValue) || '1d';
 
 const userTargetVersion =
-	(process.env.HUP_TARGET_VERSION as unknown as StringValue) || '';
+	(process.env.HUP_TARGET_VERSION as unknown as string) || '';
 
 if (!apiKey) {
 	console.error('BALENA_API_KEY required in environment');
@@ -33,28 +46,24 @@ const balena = getSdk({
 });
 
 const delay = (value: StringValue) => {
-	try {
-		return new Promise((resolve) => setTimeout(resolve, ms(value)));
-	} catch (e) {
-		throw e;
-	}
+	return new Promise((resolve) => setTimeout(resolve, ms(value)));
 };
 
-const getExpandedProp = <T, K extends keyof T>(
+const getExpandedProp = <T extends object, K extends keyof T>(
 	obj: OptionalNavigationResource<T>,
 	key: K,
-) => (Array.isArray(obj) && obj[0] && obj[0][key]) || undefined;
+) => (Array.isArray(obj) && obj[0] && obj[0][key]) ?? undefined;
 
 const getDeviceType = async (uuid: string): Promise<string> => {
 	return await balena.models.device
 		.get(uuid, { $expand: { is_of__device_type: { $select: 'slug' } } })
-		.then(async (device) => {
+		.then((device) => {
 			return getExpandedProp(device.is_of__device_type, 'slug') as string;
 		});
 };
 
 const getDeviceVersion = async (uuid: string): Promise<string> => {
-	return await balena.models.device.get(uuid).then(async (device) => {
+	return await balena.models.device.get(uuid).then((device) => {
 		return balena.models.device.getOsVersion(device);
 	});
 };
@@ -73,36 +82,51 @@ const getTargetVersion = async (
 				return null;
 			} else {
 				if (['recommended', 'latest'].includes(userTargetVersion)) {
-					return osUpdateVersions.recommended as string;
+					return osUpdateVersions.recommended!;
 				} else {
 					return (
-						(osUpdateVersions.versions.find((version: string) =>
+						osUpdateVersions.versions.find((version: string) =>
 							version.includes(userTargetVersion),
-						) as string) || null
+						)! || null
 					);
 				}
 			}
 		});
 };
 
-const getUpdateStatus = async (uuid: string): Promise<any> => {
+/** Retrieve device model for status of HUP properties. */
+const getUpdateStatus = async (uuid: string): Promise<HupStatus> => {
 	try {
-		const hupStatus = await balena.models.device.getOsUpdateStatus(uuid);
-		console.log(hupStatus);
-		return hupStatus;
+		const hupProps = await balena.models.device.get(uuid, {
+			$select: ['status', 'provisioning_state', 'provisioning_progress'],
+		});
+		console.log(`Device HUP status: ${JSON.stringify(hupProps)}`);
+
+		if (hupProps.status.toLowerCase() === 'configuring') {
+			if (hupProps.provisioning_state === 'OS update failed') {
+				return HupStatus.FAILED;
+			} else {
+				return HupStatus.RUNNING;
+			}
+		} else if (hupProps.status.toLowerCase() === 'idle') {
+			return HupStatus.NOT_RUNNING;
+		} else {
+			return HupStatus.DEVICE_BUSY;
+		}
 	} catch (e) {
 		console.error(`Error getting status: ${e}`);
-		return false;
+		return HupStatus.UNKNOWN;
 	}
 };
 
 const main = async () => {
+	const delayStates = [
+		HupStatus.UNKNOWN,
+		HupStatus.RUNNING,
+		HupStatus.DEVICE_BUSY,
+	];
 	while (true) {
-		try {
-			await balena.auth.loginWithToken(apiKey);
-		} catch (e) {
-			throw e;
-		}
+		await balena.auth.loginWithToken(apiKey);
 
 		while (!(await balena.models.device.isOnline(deviceUuid))) {
 			console.log('Device is offline...');
@@ -111,11 +135,11 @@ const main = async () => {
 
 		console.log('Checking last update status...');
 		while (
-			await getUpdateStatus(deviceUuid).then((status) => {
-				return !status || status.status === 'in_progress';
-			})
+			await getUpdateStatus(deviceUuid).then((status) =>
+				delayStates.includes(status),
+			)
 		) {
-			console.log('Another update is already in progress...');
+			console.log('Another update may be in progress...');
 			await delay('2m');
 		}
 
@@ -133,16 +157,20 @@ const main = async () => {
 		} else {
 			console.log(`Starting balenaOS host update to ${targetVersion}...`);
 			await balena.models.device
-				.startOsUpdate(deviceUuid, targetVersion)
+				.startOsUpdate(deviceUuid, targetVersion, { runDetached: true })
 				.then(async () => {
+					// Allow time for server to start HUP on device, which then
+					// sets Configuring status.
+					await delay('20s');
 					while (
-						// print progress at regular intervals until status changes
-						// or device reboots
-						await getUpdateStatus(deviceUuid).then((status) => {
-							return !status || status.status === 'in_progress';
-						})
+						// Print progress at regular intervals while API indicates
+						// HUP still may be running.
+						await getUpdateStatus(deviceUuid).then(
+							(status) =>
+								status === HupStatus.UNKNOWN || status === HupStatus.RUNNING,
+						)
 					) {
-						await delay('10s');
+						await delay('20s');
 					}
 				})
 				.catch((e) => {
@@ -157,4 +185,6 @@ const main = async () => {
 };
 
 console.log('Starting up...');
-main();
+main().catch((e) => {
+	console.error(e);
+});
