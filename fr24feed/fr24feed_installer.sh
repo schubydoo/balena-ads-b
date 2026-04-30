eebb0fdf8161862d8ff3ff0852a8d004188fa7e6
Marcus Schubert <schubydoo@users.noreply.github.com>
2026-04-30 09:49:13 -0700

    fix: use correct fr24feed download hostname in installer
    The installer was using repo-feed.flightradar24.com (hyphen) to download binaries, but FR24's canonical download URLs use repo.feed.flightradar24.com (dot). This mismatch was causing wget to return a server error (exit code 8) on upstream build workers where the Docker layer cache was cold.

--- fr24feed/fr24feed_installer.sh
+++ fr24feed/fr24feed_installer.sh
@@ -4,22 +4,22 @@ set -e
 arch="$(dpkg --print-architecture)"
 echo System Architecture: $arch
 
-if [ "$arch" = "arm64" ]; then 
+if [ "$arch" = "arm64" ]; then
 	dpkg --add-architecture armhf
 	fr24feed_arch=armhf
-	fr24feed_url="https://repo-feed.flightradar24.com/rpi_binaries/"
-	
-elif [ "$arch" = "amd64" ]; then 
+	fr24feed_url="http://repo.feed.flightradar24.com/rpi_binaries/"
+
+elif [ "$arch" = "amd64" ]; then
 	fr24feed_arch=amd64
-	fr24feed_url="https://repo-feed.flightradar24.com/linux_binaries/"
-else 
+	fr24feed_url="http://repo.feed.flightradar24.com/linux_binaries/"
+else
 	fr24feed_arch=armhf
-	fr24feed_url="https://repo-feed.flightradar24.com/rpi_binaries/"
+	fr24feed_url="http://repo.feed.flightradar24.com/rpi_binaries/"
 fi
 
 cd /tmp
 
-fr24feed_installer="fr24feed_${FR24FEED_VERSION}_$fr24feed_arch.tgz" 
+fr24feed_installer="fr24feed_${FR24FEED_VERSION}_$fr24feed_arch.tgz"
 
 wget -O fr24feed.tgz $fr24feed_url$fr24feed_installer
 
